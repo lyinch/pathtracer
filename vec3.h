f5eca1eb2db1a69e3dc335c6727895bdca33b2e4
Backes Thierry <backes.thierry@gmail.com>
2018-10-21 16:45:09 +0200

    overloaded * operator for vec3

--- vec3.h
+++ vec3.h
@@ -52,6 +52,9 @@ inline Vec3 operator- (const Vec3 &v1, const Vec3 &v2){
     return {v1.x-v2.x,v1.y-v2.y,v1.z-v2.z};
 }
 
+inline Vec3 operator* (const Vec3 &v1, const Vec3 &v2){
+    return {v1.x*v2.x,v1.y*v2.y,v1.z*v2.z};
+}
 
 inline Vec3 operator+ (const Vec3 &v1,float t){
     return {v1.x+t,v1.y+t,v1.z+t};
